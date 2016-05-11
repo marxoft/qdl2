df92c5965022cb434a14a28fe9a6b2939719968c
Stuart Howarth <showarth@marxoft.co.uk>
2016-05-11 22:12:52 +0100

    Return from startNextTransfers() once the maximum number is reached.

--- app/src/base/transfermodel.cpp
+++ app/src/base/transfermodel.cpp
@@ -628,7 +628,7 @@ void TransferModel::startNextTransfers() {
                 addActiveTransfer(transfer);
 
                 if (activeTransfers() == maximum) {
-                    break;
+                    return;
                 }
             }
         }
