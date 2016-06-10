7da24a4a97af005cbb7245e4ef6303ee3b2e24c9
Stuart Howarth <showarth@marxoft.co.uk>
2016-06-10 17:46:11 +0100

    Fix signal connection.

--- app/src/desktop/archiveextractor.cpp
+++ app/src/desktop/archiveextractor.cpp
@@ -106,8 +106,8 @@ void ArchiveExtractor::extract(const QString &password) {
             m_process = new QProcess(this);
             connect(m_process, SIGNAL(finished(int, QProcess::ExitStatus)),
                     this, SLOT(onProcessFinished(int, QProcess::ExitStatus)));
-            connect(m_process, SIGNAL(finished(QProcess::ProcessError)),
-                    this, SLOT(onProcessError(int, QProcess::ProcessError)));
+            connect(m_process, SIGNAL(error(QProcess::ProcessError)),
+                    this, SLOT(onProcessError(QProcess::ProcessError)));
         }
 
         if (m_process->state() != QProcess::Running) {
