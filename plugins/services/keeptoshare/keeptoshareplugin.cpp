8258792e2a89d8c0d0336244971f6002316afdd9
Stuart Howarth <showarth@marxoft.co.uk>
2017-08-03 20:19:31 +0100

    Fix filename detection.

--- plugins/services/keeptoshare/keeptoshareplugin.cpp
+++ plugins/services/keeptoshare/keeptoshareplugin.cpp
@@ -143,12 +143,7 @@ void KeepToSharePlugin::checkUrlIsValid() {
     }
 
     const QString response = QString::fromUtf8(reply->readAll());
-    QString fileName;
-    const QRegExp re("(File: <span>|font-size: 18px; \">|title-account\"><span><strong>)([^<]+)");
-
-    if (re.indexIn(response) != -1) {
-        fileName = re.cap(2).trimmed();
-    }
+    const QString fileName = response.section("class=\"title-file\">", 1, 1).section('<', 0, 0).trimmed();
 
     if (fileName.isEmpty()) {
         emit error(tr("File not found"));
