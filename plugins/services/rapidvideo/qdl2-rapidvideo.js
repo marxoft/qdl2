212b3d4e528daab593f69d6cb2d02f090719e77b
Stuart Howarth <showarth@marxoft.co.uk>
2017-11-05 20:36:20 +0000

    Fix video URL detection. Update changelog.

--- plugins/services/rapidvideo/qdl2-rapidvideo.js
+++ plugins/services/rapidvideo/qdl2-rapidvideo.js
@@ -14,7 +14,6 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-var FORMATS = ["720p", "480p", "360p"];
 var request = null;
 
 function checkUrl(url) {
@@ -40,43 +39,8 @@ function getDownloadRequest(url) {
     request.onreadystatechange = function() {
         if (request.readyState == 4) {
             try {
-                var response = request.responseText;
-                var options = [];
-
-                for (var i = 0; i < FORMATS.length; i++) {
-                    try {
-                        var re =
-                            new RegExp("<source src=\"([^\"]+)\" type=\"video/mp4\" title=\"" + FORMATS[i] + "\"");
-                        options.push({"label": FORMATS[i].toUpperCase(), "value": re.exec(response)[1]});
-                    }
-                    catch(e) {
-                        continue;
-                    }
-                }
-
-                if (!options.length) {
-                    error(qsTr("No video formats found"));
-                    return;
-                }
-
-                if (settings.value("useDefaultFormat", false) === true) {
-                    var format = settings.value("format", "720P");
-
-                    for (var i = 0; i < options.length; i++) {
-                        if (options[i].label == format) {
-                            downloadRequest(new NetworkRequest(options[i].value));
-                            return;
-                        }
-                    }
-
-                    downloadRequest(new NetworkRequest(options[options.length - 1].value));
-                }
-                else {
-                    var list = {"type": "list", "label": qsTr("Video format"), "key": "url", "options": options,
-                                "value": options[0].value};
-                    settingsRequest(qsTr("Choose video format"), [list],
-                        function(f) { downloadRequest(new NetworkRequest(f.url)); });
-                }
+                var source = /<source src="([^"]+)/.exec(request.responseText)[1];
+                downloadRequest(new NetworkRequest(source));
             }
             catch(e) {
                 error(e);
@@ -84,7 +48,7 @@ function getDownloadRequest(url) {
         }
     }
 
-    request.open("GET", url + "&q=0p"); // Ensure full list of formats appears
+    request.open("GET", url);
     request.send();
 }
 
