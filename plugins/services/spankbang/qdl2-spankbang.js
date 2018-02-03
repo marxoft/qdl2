d4ad5ca943c97c0af711843314c03c47d60b8b8a
Stuart Howarth <showarth@marxoft.co.uk>
2018-02-03 04:33:07 +0000

    Improve video format detection.

--- plugins/services/spankbang/qdl2-spankbang.js
+++ plugins/services/spankbang/qdl2-spankbang.js
@@ -15,13 +15,7 @@
  */
 
 (function() {
-    var VIDEO_FORMATS = ["1080p", "720p", "480p"];
     var request = null;
-
-    function getVideoUrl(streamId, streamKey, format) {
-        return "http://spankbang.com/_" + streamId + "/" + streamKey + "/title/" + format + "__mp4";
-    }
-
     var plugin = new ServicePlugin();
 
     plugin.checkUrl = function(url) {
@@ -30,19 +24,11 @@
         request.onreadystatechange = function () {
             if (request.readyState == 4) {
                 try {
-                    var streamKey = request.responseText.split("stream_key  = '")[1].split("'")[0];
-                    var streamId = request.responseText.split("stream_id  = '")[1].split("'")[0];
-                    var fileName = request.responseText.split("h1 title=\"")[1].split("\"")[0];
-                    
-                    if ((streamKey) && (streamId) && (fileName)) {
-                        plugin.urlChecked(new UrlResult(url, fileName + ".mp4"));
-                    }
-                    else {
-                        plugin.error(qsTr("File not found"));
-                    }
+                    var fileName = decodeHtml(/h1 title="([^"]+)/.exec(request.responseText)[1]) + ".mp4";
+                    plugin.urlChecked(new UrlResult(url, fileName));
                 }
-                catch(err) {
-                    plugin.error(err);
+                catch(e) {
+                    plugin.error(e);
                 }
             }
         }
@@ -57,47 +43,46 @@
         request.onreadystatechange = function () {
             if (request.readyState == 4) {
                 try {
-                    var streamKey = request.responseText.split("stream_key  = '")[1].split("'")[0];
-                    var streamId = request.responseText.split("stream_id  = '")[1].split("'")[0];
-                    
-                    if ((!streamKey) || (!streamId)) {
-                        plugin.error(qsTr("File not found"));
+                    var response = request.responseText;
+                    var formats = [];
+                    var re = /stream_url_(\d+(p|k))\s+=\s+'([^']+)/g;
+                    var match;
+
+                    while (match = re.exec(response)) {
+                        try {
+                            formats.unshift({"label": match[1].toUpperCase(), "value": match[3]});
+                            re.lastIndex = match.index + match[0].length;
+                        }
+                        catch(e) {
+                            re.lastIndex = match.index + match[0].length;
+                        }
+                    }
+
+                    if (!formats.length) {
+                        plugin.error(tr("No video formats found"));
                         return;
                     }
                     
-                    var shd = parseInt(request.responseText.split("stream_shd  = ")[1].split(";")[0]);
-                    var hd = parseInt(request.responseText.split("stream_hd  = ")[1].split(";")[0]);
-                    var formats = VIDEO_FORMATS;
-                    
-                    if (shd != 1) {
-                        formats.splice(0, 1);
-                    }
-                    
-                    if (hd != 1) {
-                        formats.splice(0, 1);
-                    }
-                    
-                    var format = settings.videoFormat || "1080p";
+                    var format = settings.videoFormat || "4K";
 
                     if (settings.useDefaultVideoFormat) {
-                        var videoUrl = getVideoUrl(streamId, streamKey, formats[Math.max(0, formats.indexOf(format))]);
-                        plugin.downloadRequest(new NetworkRequest(videoUrl));
-                    }
-                    else {
-                        var options = [];
-                        
                         for (var i = 0; i < formats.length; i++) {
-                            var videoUrl = getVideoUrl(streamId, streamKey, formats[i]);
-                            options.push({"label": formats[i].toUpperCase(), "value": videoUrl});
+                            if (formats[i].label == format) {
+                                plugin.downloadRequest(new NetworkRequest(formats[i].value));
+                                return;
+                            }
                         }
 
+                        plugin.downloadRequest(new NetworkRequest(formats[0].value));
+                    }
+                    else {
                         plugin.settingsRequest(qsTr("Video format"), [{"type": "list", "label": qsTr("Video format"),
-                            "key": "url", "value": options[Math.max(0, options.indexOf(format))].value,
-                            "options": options}], function (f) { plugin.downloadRequest(new NetworkRequest(f.url)); });
+                            "key": "url", "value": formats[Math.max(0, formats.indexOf(format))].value,
+                            "options": formats}], function (f) { plugin.downloadRequest(new NetworkRequest(f.url)); });
                     }
                 }
-                catch(err) {
-                    plugin.error(err);
+                catch(e) {
+                    plugin.error(e);
                 }
             }
         }
